5a359f463f5d3ba3c495b7e91c4cd07c7ab616e9
WAJDI RIAHI <rwajdi2020@gmail.com>
2023-08-17 23:35:45 +0200

    btn

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,9 @@ import styles from "./Button.module.css";
 
 function Button({ children, onClick, type }) {
   return (
-    <button className={`${styles.btn} ${styles[type]}`}>{children}</button>
+    <button onClick={onClick} className={`${styles.btn} ${styles[type]}`}>
+      {children}
+    </button>
   );
 }
 
